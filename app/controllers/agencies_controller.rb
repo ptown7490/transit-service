424c603fa4f796ff40a22b509e73f315e1007a2e
Patrick McGreevy <patrick7490@icloud.com>
2017-08-25 23:13:19 -0700

    Complete AgenciesController

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -5,8 +5,32 @@ class AgenciesController < ApplicationController
     json_response(@agencies)
   end
 
+  def show
+    @agency = Agency.find(params[:id])
+    json_response(@agency)
+  end
+
+  def create
+    @agency = Agency.create(agency_params)
+    json_response(@agency)
+  end
+
+  def update
+    @agency = Agency.find(params[:id])
+    @agency.update(agency_params)
+  end
+
+  def destroy
+    @agency = Agency.find(params[:id])
+    @agency.destroy
+  end
+
   private
   def json_response(object)
     render json: object, status: :ok
+  end
+
+  def agency_params
+    params.require(:agency).permit(:name)
   end
 end
