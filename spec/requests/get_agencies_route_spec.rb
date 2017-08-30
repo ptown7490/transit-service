d0ed190b78c8f249dcb58348ebc32bffe351be12
Patrick McGreevy <patrick7490@icloud.com>
2017-08-29 19:24:14 -0700

    Add tests for GET ‘/agencies’ route’s response

---
+++ spec/requests/get_agencies_route_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "get all agencies route", type: :request do
+  let!(:agencies) { Agency.create(name: 'TriMet') }
+
+  before { get '/agencies' }
+
+  it 'returns all agencies' do
+    expect(JSON.parse(response.body).size).to eq 1
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
