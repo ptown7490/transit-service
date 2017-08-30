1f55ca916ccf9aa57c1ec448661b50af5e6d79fc
Patrick McGreevy <patrick7490@icloud.com>
2017-08-30 01:27:15 -0700

    Add TriMet’s WES route to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,21 @@ class Seed
   def self.begin
     seed = Seed.new
     seed.generate_agencies
+    seed.generate_routes(1)
   end
 
   def generate_agencies
-    Agency.create!(name: 'TriMet')
+    Agency.create!(name: 'TriMet',
+                   id:    1
+                   )
+  end
+
+  def generate_routes(agency_id)
+    Route.create!(agency_id:  agency_id.to_i,
+                  local_id:   203,
+                  name:       'WES Commuter Rail',
+                  short_name: 'WES'
+                  )
   end
 
 end
