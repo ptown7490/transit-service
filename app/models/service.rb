a7c82feda0a773802eb53e7488bfdbb7f071e557
Patrick McGreevy <patrick7490@icloud.com>
2017-11-30 20:41:18 -0800

    Fix issue with validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,11 +2,4 @@ class Service < ActiveRecord::Base
   belongs_to :agency
   has_many :trips
 
-  validates :monday, presence: true
-  validates :tuesday, presence: true
-  validates :wednesday, presence: true
-  validates :thursday, presence: true
-  validates :friday, presence: true
-  validates :saturday, presence: true
-  validates :sunday, presence: true
 end
--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -3,11 +3,4 @@ require 'rails_helper'
 describe Service do
   it { should belong_to :agency }
   it { should have_many :trips }
-  it { should validate_presence_of :monday }
-  it { should validate_presence_of :tuesday }
-  it { should validate_presence_of :wednesday }
-  it { should validate_presence_of :thursday }
-  it { should validate_presence_of :friday }
-  it { should validate_presence_of :saturday }
-  it { should validate_presence_of :sunday }
 end
