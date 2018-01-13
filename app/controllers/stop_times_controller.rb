75cf01aefd6c92260231fbc39d9f739fdd0d8a08
Patrick McGreevy <patrick7490@icloud.com>
2018-01-13 12:31:12 -0800

    Add detail to controllers

--- app/controllers/stop_times_controller.rb
+++ app/controllers/stop_times_controller.rb
@@ -4,6 +4,15 @@ class StopTimesController < ApplicationController
     if params[:trip_id]
       @trip = Trip.find(params[:trip_id])
       @stop_times = @trip.stop_times.order(:trip_id, :stop_sequence)
+    elsif params[:route_direction_id]
+      @rd = RouteDirection.find(params[:route_direction_id])
+      @stop_times = []
+      if params[:service_id]
+        trips = @rd.trips.where(service_id: params[:service_id])
+        trips.each do |trip|
+          @stop_times << trip.stop_times.order(:stop_sequence)
+        end
+      end
     else
       @stop_times = StopTime.all.order(:trip_id, :stop_sequence).limit(50)
     end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,14 +1,30 @@
 class TripsController < ApplicationController
 
   def index
+    if params[:service_id]
+      @service_id = params[:service_id]
+    end
+
     if params[:route_id]
       @route = Route.find(params[:route_id])
-      @trips = @route.trips
+      if ! @service_id.nil?
+        @trips = @route.trips.where(service_id: @service_id).order(:block_id)
+      else
+        @trips = @route.trips.distinct.select(:service_id)
+      end
     elsif params[:route_direction_id]
       @rd = RouteDirection.find(params[:route_direction_id])
-      @trips = @rd.trips
+      if ! @service_id.nil?
+        @trips = @rd.trips.where(service_id: @service_id)
+      else
+        @trips = @rd.trips
+      end
     else
-      @trips = Trip.all.order(:block_id)
+      if ! @service_id.nil?
+        @trips = Trip.where(service_id: @service_id).order(:block_id)
+      else
+        @trips = Trip.all.order(:block_id)
+      end
     end
     json_response(@trips)
   end
