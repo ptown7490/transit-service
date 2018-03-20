4c7c0c266c141b7fbddeae7d0a2b2c2b7bede416
Patrick McGreevy <patrick7490@icloud.com>
2018-03-19 19:14:08 -0700

    Add basic views for Stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -3,19 +3,27 @@ class StopsController < ApplicationController
   def index
     if params[:agency_id]
       @agency = Agency.find(params[:agency_id])
-      @stops = @agency.stops
+      @stops = @agency.stops.order(:location_class, :id)
     elsif params[:trip_id]
       @trip = Trip.find(params[:trip_id])
       @stops = @trip.stops
     else
       @stops = Stop.all
     end
-    json_response(@stops)
+
+    respond_to do |format|
+      format.html
+      format.json { json_response(@stops) }
+    end
   end
 
   def show
     @stop = Stop.find(params[:id])
-    json_response(@stop)
+
+    respond_to do |format|
+      format.html
+      format.json { json_response(@stop) }
+    end
   end
 
 end
