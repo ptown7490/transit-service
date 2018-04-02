fe443313bc84bf2e22b59fb91da9c3b654878d52
Patrick McGreevy <patrick7490@icloud.com>
2018-04-02 14:22:04 -0700

    Add link to docs on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @documentation_url = "https://app.swaggerhub.com/apis/ptown1690/transit-service/1.0#/"
     @agencies = Agency.all
   end
 
